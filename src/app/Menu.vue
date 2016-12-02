cb3dbfa2b5c76ea87abb1dec86face5bf4b1f2f3
victor rebiard--crépin <victor rebiard--crpin>
2016-12-02 17:12:45 +0100

    Menu buttons

--- src/app/Menu.vue
+++ src/app/Menu.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="menu">
     <ul class="menu-items">
-      <li class="menu-item" v-for="item in items" v-bind:class="{ 'selected': ($route.fullPath === item.route) }">
-        <router-link :to="{ path: item.route }">{{ item.title }}</router-link>
+      <li class="menu-item" v-for="item in items" v-bind:class="{ 'selected': ($route.fullPath === item.route) }" v-on:click="go(item.route)">
+        <span>{{ item.title }}</span>
       </li>
     </ul>
   </div>
@@ -14,12 +14,17 @@ export default {
   data() {
     return {
       items: [
-        {title: 'Dashboard', route: '/dashboard', isSelected: true},
-        {title: 'Commandes', route: '/orders', isSelected: false},
-        {title: 'Pizzas', route: '/pizzas', isSelected: false},
-        {title: 'Bases', route: '/bases', isSelected: false}
+        {title: 'Dashboard', route: '/dashboard'},
+        {title: 'Commandes', route: '/orders'},
+        {title: 'Pizzas', route: '/pizzas'},
+        {title: 'Bases', route: '/bases'}
       ]
     };
+  },
+  methods: {
+    go(route) {
+      this.$router.push(route);
+    }
   }
 };
 </script>
